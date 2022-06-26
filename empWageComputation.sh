2ba206e694fa5f73e5d5bc47a6fe0822eb42873b
lovelotus <opera.prem@gmail.com>
2022-06-26 22:31:36 +0530

    implementation of emp attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to emp wage"
 
+IS_PRESENT=1
+IS_ABSENT=0
+randomValue=0
+presentAbsent=-1
+
+
+echo "Welcome to emp wage"
+
+randomValue=$((RANDOM))
+presentAbsent=$(($randomValue%2))
+
+if [ $presentAbsent -eq $IS_PRESENT ]
+then
+	echo "Emp present"
+else
+	echo "Emp absent"
+fi
